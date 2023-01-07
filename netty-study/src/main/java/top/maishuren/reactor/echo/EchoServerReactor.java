6dcf32c65849f18511523da9a67a0e453d658d7b
maishuren <maisrcn@qq.com>
2023-01-07 17:00:08 +0800

    single thread reactor

--- netty-study/src/main/java/top/maishuren/reactor/echo/EchoServerReactor.java
+++ netty-study/src/main/java/top/maishuren/reactor/echo/EchoServerReactor.java
@@ -1,10 +1,37 @@
 package top.maishuren.reactor.echo;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
- * 单线程版 Reactor
+ * 多线程版 Reactor
+ *
  * @author MaiShuRen
  * @site <a href="https://www.maishuren.top">maiBlog</a>
  * @since 2023-01-06 23:52
  **/
 public class EchoServerReactor {
+
+    ServerSocketChannel serverSocketChannel;
+    AtomicInteger next = new AtomicInteger(0);
+    Selector[] selectors = new Selector[2];
+
+    public EchoServerReactor() throws IOException {
+        // 用于监听连接事件
+        selectors[0] = Selector.open();
+        // 用于监听传输事件
+        selectors[1] = Selector.open();
+
+        serverSocketChannel = ServerSocketChannel.open();
+        InetSocketAddress address = new InetSocketAddress("127.0.0.1,", 18888);
+        serverSocketChannel.socket().bind(address);
+        serverSocketChannel.configureBlocking(false);
+        SelectionKey selectionKey = serverSocketChannel.register(selectors[0], SelectionKey.OP_ACCEPT);
+
+
+    }
 }
